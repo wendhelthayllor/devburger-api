16f2bba7c38a166fd772e860084893d4bd549450
wendhelthayllor <wendheldev@gmail.com>
2024-12-15 11:12:02 -0300

    criando uma funcionalidade de pagamento

--- src/app/controllers/CreatePaymentIntentController.js
+++ src/app/controllers/CreatePaymentIntentController.js
@@ -9,7 +9,7 @@ const calculateOrderAmount = (items) => {
     return current.price * current.quantity + acc
   }, 0)
 
-  return total * 100;
+  return total;
 }
 
 class CreatePaymentIntentController {
